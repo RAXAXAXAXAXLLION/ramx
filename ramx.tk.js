add05e9bb6e5fd53e887fd3fcb06e5ceed09cc13
RAXAAXXAXAXAXAXXAXAXAXAXXAXAXAXAXLLLLLLLLLIÓÒÓÒÓÒÓÒ´´O``´`´`´´´´`´Ò´`´`´`O <ramxaxlon@gmail.com>
2017-08-02 22:16:31 +0200

    Update ramx.tk.js

--- ramx.tk.js
+++ ramx.tk.js
@@ -14,12 +14,11 @@ onkeydown=onkeyup=(e)=>{k[e.keyCode]=e.type=="keydown"}
 id=Math.floor(Math.random()*Math.pow(10,10))
 document.body.style.margin="0 0 0 0"
 input={x:0}
-onkeydown=()=>{}
 setInterval(()=>{
 //c.width=innerWidth
 //c.height=innerHeight
 document.body.innerHTML=""
-firebase.database().ref(id).set({x:input.x})
-if(k[37]){input.x-=1}
+//firebase.database().ref(id).set({x:input.x})
+if(k[37]){input.x-=1;console.log("Left pressed.")}
 if(k[39]){input.x+=1}
 },1)

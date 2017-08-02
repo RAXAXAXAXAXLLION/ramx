2cbf2d5792a719d29bb464a93f263f1cb5935b19
RAXAAXXAXAXAXAXXAXAXAXAXXAXAXAXAXLLLLLLLLLIÓÒÓÒÓÒÓÒ´´O``´`´`´´´´`´Ò´`´`´`O <ramxaxlon@gmail.com>
2017-08-02 19:56:43 +0200

    Update ramx.tk.js

--- ramx.tk.js
+++ ramx.tk.js
@@ -6,18 +6,19 @@ apiKey:"AIzaSyDjmCrbhZxIvgnwqID_VJ4qH6rIYroGXrk",
 storageBucket:"project-9156400537247143604.appspot.com",
 authDomain:"project-9156400537247143604.firebaseapp.com",
 databaseURL:"https://project-9156400537247143604.firebaseio.com"})
+firebase.database().ref('/').on('value',(e)=>{out=e.val()})
 onkeydown=onkeyup=(e)=>{k[e.keyCode]=e.type=="keydown"}
 id=Math.floor(Math.random()*Math.pow(10,10))
 document.body.style.margin="0 0 0 0"
-firebase.database().ref(id).set({x:0})
-firebase.database().ref('/').on('value',(e)=>{out=e.val()})
+in={x:0}
 onkeydown=()=>{}
 setInterval(()=>{
 //c.width=innerWidth
 //c.height=innerHeight
 document.body.innerHTML=""
-if(k[37]){firebase.database().ref(id).set({x:out[id].x-1})}
-if(k[39]){firebase.database().ref(id).set({x:out[id].x+1})}
+firebase.database().ref(id).set({x:in.x})
+if(k[37]){in.x-=1}
+if(k[39]){in.x+=1}
 for(i in out){
 document.write(out[i].x)
 }},1)

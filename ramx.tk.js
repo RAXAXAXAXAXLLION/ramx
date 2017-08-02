bbcef9e5d657c035618c193ed9e1de264fb51c2b
RAXAAXXAXAXAXAXXAXAXAXAXXAXAXAXAXLLLLLLLLLIÓÒÓÒÓÒÓÒ´´O``´`´`´´´´`´Ò´`´`´`O <ramxaxlon@gmail.com>
2017-08-02 19:05:11 +0200

    Update ramx.tk.js

--- ramx.tk.js
+++ ramx.tk.js
@@ -1,12 +1,13 @@
-id=Math.floor(Math.random()*Math.pow(10,10))
+k=[]
 firebase.initializeApp({
-apiKey:"AIzaSyDjmCrbhZxIvgnwqID_VJ4qH6rIYroGXrk",
-authDomain:"project-9156400537247143604.firebaseapp.com",
-databaseURL:"https://project-9156400537247143604.firebaseio.com",
+messagingSenderId:"709939686799",
 projectId:"project-9156400537247143604",
+apiKey:"AIzaSyDjmCrbhZxIvgnwqID_VJ4qH6rIYroGXrk",
 storageBucket:"project-9156400537247143604.appspot.com",
-messagingSenderId:"709939686799"})
+authDomain:"project-9156400537247143604.firebaseapp.com",
+databaseURL:"https://project-9156400537247143604.firebaseio.com"})
 onkeydown=onkeyup=(e)=>{k[e.keyCode]=e.type=="keydown"}
+id=Math.floor(Math.random()*Math.pow(10,10))
 document.body.style.margin="0 0 0 0"
 firebase.database().ref(id).set({x:0})
 firebase.database().ref('/').on('value',(e)=>{output=e.val()})
@@ -14,6 +15,8 @@ onkeydown=()=>{}
 setInterval(()=>{
 //c.width=innerWidth
 //c.height=innerHeight
+if(k[37]){firebase.database().ref(id).set({x:output[id].x-1})}
+if(k[39]){firebase.database().ref(id).set({x:output[id].x+1})}
 document.body.innerHTML=""
 for(i in output){
 document.write(output[i].x)
